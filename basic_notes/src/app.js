726cc9a8e4b46ab9f936295ac1b1ddaff8b4b36a
syed umer <umershakir987@gmail.com>
2026-07-22 11:12:51 +0500

    Adding code

--- basic_notes/src/app.js
+++ basic_notes/src/app.js
@@ -17,4 +17,27 @@ app.get('/notes',(req,res)=>{
         notes:notes
     })
 })
+
+app.delete('/notes/:index',(req,res)=>{
+    const idx = req.params.index
+    delete notes[idx]  //deleting notes
+    res.status(200).json({
+        message:"idx delete successfully remaings are",
+        notes:notes
+    })
+})
+
+
+app.patch('/notes/:index',(req,res)=>{
+    const idx = req.params.index
+    const desc = req.body.desc
+    const tit = req.body.title
+    notes[idx].desc = desc
+    notes[idx].title = tit
+
+    res.status(200).json({
+        message:"successfully update"
+    })
+})
+
 module.exports = app
