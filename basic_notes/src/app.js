1bc51e5036f5b63053c0419681847622062101a7
syed umer <umershakir987@gmail.com>
2026-07-22 10:13:20 +0500

    adding another lesson

--- basic_notes/src/app.js
+++ basic_notes/src/app.js
@@ -1,11 +1,11 @@
 const express =require('express')
 
 const app = express()
-
+app.use(express.json())
 const notes = []
 
 app.post('/notes' , (req,res)=>{
-    
+    console.log(req.body)
 })
 
 
