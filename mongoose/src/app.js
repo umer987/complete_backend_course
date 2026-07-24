7d88c3752d60c505888b65c6b833e74f4eba1b69
syed umer <umershakir987@gmail.com>
2026-07-24 11:51:44 +0500

    adding mongodb

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -1,9 +1,18 @@
 const express =require('express')
 const app = express()
 const notemodel = require('./db/Models/notes.model')
+app.use(express.json())
 
-
-
+app.post('/note',async (req,res)=>{
+    const data = req.body
+    await notemodel.create({
+        title:data.title,
+        desc:data.desc
+    })
+    res.status(201).json({
+        message: "note created successfully"
+    })
+})
 
 
 module.exports = app
