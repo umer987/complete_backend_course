15cd0796d2f5378c1d5686deebf839bfe9c6674c
syed umer <umershakir987@gmail.com>
2026-07-24 12:08:26 +0500

    adding mongodb

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -5,12 +5,14 @@ app.use(express.json())
 
 app.post('/note',async (req,res)=>{
     const data = req.body
+    console.log(data)
     await notemodel.create({
         title:data.title,
-        desc:data.desc
+        desc:data.desc,
     })
     res.status(201).json({
         message: "note created successfully"
+        
     })
 })
 
