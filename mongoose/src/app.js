b40d548c613efb6e3b35069761e3eeea024c200e
syed umer <umershakir987@gmail.com>
2026-07-25 09:30:24 +0500

    adding insta replica and complrting mongoose

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -16,5 +16,34 @@ app.post('/note',async (req,res)=>{
     })
 })
 
+app.get('/note',async (req,res)=>{
+//const data = await notemodel.find({ title:'new one'}) condition in find also put it in findOne() 
+  const data = await notemodel.find()  //also findOne() for find specific item
+    res.status(201).json({
+        message:"data fetched",
+        data:data
+    })
+})
 
+app.delete('/note/:id',async (req ,res) => {
+    const id =req.params.id
+    await notemodel.findOneAndDelete({
+        _id:id
+    })
+    const data = await notemodel.find()
+    res.status(201).json({
+        message: "record deleted kindly review",
+        data:data
+    })
+})
+
+app.patch('/note/:id', async (req,res) => {
+    const id = req.params.id
+    const desc = req.body.desc
+    await notemodel.findOneAndUpdate({_id:id },{desc:desc})
+     res.status(201).json({
+        message: "record deleted kindly review",
+    })
+
+})
 module.exports = app
