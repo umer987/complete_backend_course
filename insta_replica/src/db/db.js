cb96ec1cc0fa6f2a8a00cac920644e51f818740a
syed umer <umershakir987@gmail.com>
2026-07-25 09:39:57 +0500

    adding insta replica

--- insta_replica/src/db/db.js
+++ insta_replica/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connect_db() {
-    await  mongoose.connect('mongodb+srv://umershakir987_db_user:H421TNMM7RV7bW5p@try.ujkebyk.mongodb.net/insta')
+    await  mongoose.connect('mongodb+srv://umershakir987_db_user:H421TNMM7RV7bW5p@try.ujkebyk.mongodb.net/instadb')
     console.log("BD CONNECTED")
 }
 
