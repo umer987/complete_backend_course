820482a898dc53fa44ed71308d0380758300701b
syed umer <umershakir987@gmail.com>
2026-07-29 02:34:42 +0500

    adding full code

--- auth/src/controller/auth.controller.js
+++ auth/src/controller/auth.controller.js
@@ -5,3 +5,4 @@ const jwt = require("jsonwebtoken")
 async function register(req, res) {
     console.log('REGISTER BODY:', req.body)
     const { username, email, password } = req.body
+    const isuserexist = await usermodel.findOne({
