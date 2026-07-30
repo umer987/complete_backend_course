6cbdbadade603cae7fe9357a36c31403a1e3608e
syed umer <umershakir987@gmail.com>
2026-07-31 01:46:54 +0500

    adding spotify code

--- spotify_project/src/controller/auth.controller.js
+++ spotify_project/src/controller/auth.controller.js
@@ -7,3 +7,6 @@ async function register_user(req, res) {
     const { username, email, password, role = "user" } = req.body
     const hash = await bcrypt.hash(password,10)
     const isuserexist = await usermodel.findOne({
+        $or: [
+            { username },
+            { email }
