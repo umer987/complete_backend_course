7d67112124115bf8da45a3f43fec3ab325d965c2
syed umer <umershakir987@gmail.com>
2026-07-31 01:47:09 +0500

    adding spotify code

--- spotify_project/src/controller/auth.controller.js
+++ spotify_project/src/controller/auth.controller.js
@@ -10,3 +10,8 @@ async function register_user(req, res) {
         $or: [
             { username },
             { email }
+        ]
+    })
+    if (isuserexist) {
+        return res.status(409).json({ message: "USER ALREASDY EXIST" })
+    }
