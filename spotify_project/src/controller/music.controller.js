2390e20f515acc279619eba559418288de7f164b
syed umer <umershakir987@gmail.com>
2026-08-01 00:20:02 +0500

    adding full code

--- spotify_project/src/controller/music.controller.js
+++ spotify_project/src/controller/music.controller.js
@@ -2,3 +2,7 @@ const jwt = require("jsonwebtoken");
 const musicmodel = require("../db/models/music.model");
 const { uploadfile } = require("../services/storage.service");
 const albummodel  = require("../db/models/album.model");
+
+async function createmusic(req, res) {
+    
+    const {title} = req.body
