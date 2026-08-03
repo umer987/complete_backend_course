77f92f4e03413e0164876ff04b9e2154547b0475
syed umer <umershakir987@gmail.com>
2026-08-03 09:14:30 +0500

    open fintech project

--- Backend_Banking/server.js
+++ Backend_Banking/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const app = require('./src/app')
-const connect_db = require('./src/config')
+const connect_db = require('./src/config/db')
 
 connect_db()
 app.listen(3000,()=>{
