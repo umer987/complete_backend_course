709e8346ac0c1df09d576db456f0e3df2d329c03
syed umer <umershakir987@gmail.com>
2026-08-05 00:08:50 +0500

    adding more code

--- Backend_Banking/src/middleware/auth.middleware.js
+++ Backend_Banking/src/middleware/auth.middleware.js
@@ -1,3 +1,6 @@
 const usermodel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 
+async function authmiddleware(req, res, next) {
+    const authHeader = req.headers.authorization || ''
+
